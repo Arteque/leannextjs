68541be945c78fefd5414779610733f5a2403ce4
Ahmed Lemssiah <art3que@gmail.com>
2026-09-21 00:17:33 +0200

    Clean the Folders and add a todolist

---
+++ MyAppToDoList.md
@@ -0,0 +1,4 @@
+# App Fratures.
+- App Feed (Admins, Trainers) Trainees ( Read, Like perhaps Comments)
+- Trainess List (Admins, Trainers)
+- Events [Loop Events, One Time Events] (Admins, Trainers) Trainees (Read, Like perhaps Comments)
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Oswald, Libre_Franklin } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const oswaldSans = Oswald({
+  variable: "--font-oswald-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const librefranklin = Libre_Franklin({
+  variable: "--font-libre-sans",
   subsets: ["latin"],
 });
 
@@ -21,7 +21,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${oswaldSans.variable} ${librefranklin.variable} h-full antialiased`}
+      suppressHydrationWarning
+      cz-shortcut-listen="true"
+      
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
